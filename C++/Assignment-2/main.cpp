86c4e38d7289e64879c13510967ba34dfc414dd9
tarık bayer <31344520+tarikbayer@users.noreply.github.com>
2021-09-25 12:26:27 +0200

    Update main.cpp

--- C++/Assignment-2/main.cpp
+++ C++/Assignment-2/main.cpp
@@ -1,15 +1,4 @@
-/* Dateiname: main.cpp:
-Enthält das Hauptprogramm laut Aufgabenstellung
 
-AD-Praktikum SoSe 2021
-Gruppe: 20
-Schmidt, Nicole, Mat.-Nr.: 11148335
-Bayer, Tarik, Mat.-Nr.: 11149643
-Abgabe am: 21.6.21
-Praktikumsblatt: 2.1
-Compiler Flags: -
-Linker libraries/options: -
-*/
 #include <iostream>
 #include <string>
 #include "EVKD.h"
