666c65496acaa507dcf0b624591c45455a9b4d1d
tarık bayer <31344520+tarikbayer@users.noreply.github.com>
2021-09-25 12:10:10 +0200

    Update and rename ArrayGList (1).java to ArrayGList.java

--- Java/Assignment-4/ArrayGList.java
+++ Java/Assignment-4/ArrayGList.java
@@ -5,19 +5,19 @@ public class ArrayGList<T> implements GList<Object> {
 	private T[] arrayList = null;
 	private int length;
 
-	//Konstruktor, der den Array erzeugt
+	//Constructor that creates the array
 	@SuppressWarnings("unchecked")
 	public ArrayGList(int length) {
 		this.arrayList = (T[]) new Object[length];
 		this.length = 0;
 	}
 	
-	//getLength-Methode
+	//getLength-Method
 	public int getLength() {
 		return length;
 	}
 
-	//insertLast-Methode, die an der letzten Stelle der Liste einen Wert einfügt
+	//insertLast method that inserts a value at the end of the list
 	@SuppressWarnings("unchecked")
 	public int insertLast(Object value) {
 		if(arrayList.length > length) {			
@@ -28,14 +28,14 @@ public class ArrayGList<T> implements GList<Object> {
 		return -1;
 	}
 
-	//getFirst-Methode, die falls möglich, den Wert an der ersten Stelle zurückgibt
+	//getFirst method, which, if possible, returns the value in the first position
 	public Object getFirst() {
 		if(length == 0) return -9999;
 		
 		return arrayList[0];
 	}
 
-	//deletFirst-Methode, die den Wert an der ersten Stelle löscht (falls möglich) und die restlichen Werte einen Platz im Array nach vorne rückt
+	//deleteFirst method, which deletes the value in the first position (if possible) and moves the remaining values one place forward in the array
 	public int deleteFirst() {
 		if(length == 0) return -1;
 		
@@ -47,10 +47,10 @@ public class ArrayGList<T> implements GList<Object> {
 		return 0;
 	}
 
-	//Search-Methode, die mithilfe einer for-Schleife den Array durchläuft und nach der übergebenen Zahl sucht
+	//Search method that iterates through the array using a for loop and searches for the passed number
 	public boolean search(Object value, int datentyp) {
 		
-		//Search-Methode für Integer Werte, so wie in Aufgabe 3.1
+		//Search method for integer values
 		if(datentyp == 1) {
 			for(int i = 0; i < length; i++) {
 				if(value == arrayList[i]) return true;
@@ -58,7 +58,7 @@ public class ArrayGList<T> implements GList<Object> {
 			return false;
 		}
 		
-		//Search-Methode für Double Werte, mithilfe der compare Methode, da "==" nicht funktioniert hat
+		//Search method for double values, using the compare method, because "==" did not work
 		else {
 			for(int i = 0; i < length; i++) {
 				if(Double.compare((Double) value, (Double) arrayList[i]) == 0) return true;
@@ -67,7 +67,7 @@ public class ArrayGList<T> implements GList<Object> {
 		}
 	}
 
-	//Print-Methode
+	//Print-Method
 	public void print() {
 		
 		System.out.println("Inhalt der IntegerList:");
